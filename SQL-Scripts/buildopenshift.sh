5661bcfec060dffa2ca7ad1c47b886971244e39c
LUCA MARTINELLI <luca.martinelli-CIC-IT@it.ibm.com>
2020-11-09 22:36:08 +0100

    Edited build for CodeReady Container kit

--- SQL-Scripts/buildopenshift.sh
+++ SQL-Scripts/buildopenshift.sh
@@ -1,8 +1,8 @@
 echo \*\*\* YOU MUST BE LOGGED WITH DOCKER AND OC-CLI INSIDE TO OPENSHIFT CLUSTER FOR RUN THIS, DO YOU WANT CONTINUE? \*\*\*
 read -p "Press any key to continue..."
-docker build --tag default-route-openshift-image-registry.apps-crc.testing/test/mysql-simple-site .
-docker push default-route-openshift-image-registry.apps-crc.testing/test/mysql-simple-site
-oc new-app --docker-image=1default-route-openshift-image-registry.apps-crc.testing/simple-site/mysql-simplesite-image --name=mysql-simplesite
+docker build --tag default-route-openshift-image-registry.apps-crc.testing/simple-site/mysql-simple-site .
+docker push default-route-openshift-image-registry.apps-crc.testing/simple-site/mysql-simple-site
+oc new-app --docker-image=default-route-openshift-image-registry.apps-crc.testing/simple-site/mysql-simplesite-image --name=mysql-simplesite
 sleep 5
 oc get pods
 echo \*\*\* DATABASE SHOULD BE CREATED ON OPENSHIFT CLUSTER \*\*\*
